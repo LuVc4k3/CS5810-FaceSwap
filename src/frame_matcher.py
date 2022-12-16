10db138fe7a57ff9b4086a9a1666f040cac47cba
Vyaas Valsaraj <93416404+Vyaas99@users.noreply.github.com>
2022-12-15 20:42:32 -0500

    Add files via upload

---
+++ src/frame_matcher.py
@@ -0,0 +1,64 @@
+import cv2
+import numpy as np
+
+def frame_matcher(a,b):
+    num_a=len(a)
+    num_b=len(b)
+    indexes=[0]
+    a_m_b=[]
+    if num_a==num_b:
+        return a,b
+    if num_a>num_b:
+        temp=b
+        b=a
+        a=temp
+    num_a=len(a)
+    num_b=len(b)
+        
+        
+    a_m_b=[0 for i in range(num_b)]
+    a_m_b[0]=a[0].astype("uint8")
+    
+            
+    a_m_b[len(a_m_b)-1]=a[len(a)-1].astype("uint8")
+    for i in range(1,num_a-1):
+        index=int(round(((i/num_a)*num_b),0))+1
+        a_m_b[index]=a[i].astype("uint8")
+        indexes.append(index)
+    indexes.append(num_b-1)
+    index=1
+    print("indexes",indexes)
+    for i in range(1,len(indexes)):
+        batch_range=indexes[i]-indexes[i-1]
+        index=indexes[i-1]+1
+        for j in range(index,index+batch_range):
+            half=batch_range/2
+            if j<=half:
+                a_m_b[j]=a_m_b[indexes[i-1]].astype("uint8")
+            else:
+                a_m_b[j]=a_m_b[indexes[i]].astype("uint8")
+    for i in range(len(b)):
+        if b[i] is None:
+            if i!=0:
+                b[i]=b[i-1]
+            else:
+                b[i]=b[i+1]
+    for i in range(len(a_m_b)):
+        if a_m_b[i] is None:
+            if i!=0:
+                a_m_b[i]=a_m_b[i-1]
+            else:
+                a_m_b[i]=a_m_b[i+1]
+            
+
+    
+     
+    if num_a>num_b:
+        return b,a_m_b
+    else:
+        return a_m_b,b
+    
+                
+            
+        
+        
---
+++ src/video_matcher.py
@@ -0,0 +1,124 @@
+import argparse
+
+import numpy as np
+import cv2
+import time
+from helpers import *
+from frame_matcher import *
+
+
+class face_vid():
+    def __init__(self, vid_path):
+        self.path = vid_path
+        self.cap = cv2.VideoCapture(self.path)
+        self.out = cv2.VideoWriter(
+            self.path + "output.avi",
+            cv2.VideoWriter_fourcc('M','J','P','G'), 
+            25, 
+            (1280,720))  
+
+
+    def release(self):
+        self.cap.release()
+        self.out.release()
+
+
+
+
+
+#simplify transformation so that we dont need to use all convex hall
+#emotion animation machine learning
+# thin plate spline - check with the professor
+# interpolate frame if teh frame count is different 
+# rewind or stochastic select older frames in order to account for frame count mix match
+# simple is best
+
+if __name__ == '__main__':
+    # TODO: Handling of vids with different duration?
+    vid_A = face_vid(r"C:\Users\vyaas\OneDrive\Desktop\CS5810-FaceSwap-main(1)\src\Clipped video.mp4")
+    vid_B = face_vid(r"C:\Users\vyaas\OneDrive\Desktop\CS5810-FaceSwap-main(1)\src\Mrrobot-1_formatted_11-17-2022_22_.m4v")
+
+
+    frame_count = 0
+    vid_A_finished, vid_B_finished = False, False
+    a=[]
+    b=[]
+
+    print("For A")
+
+    while (vid_A.cap.isOpened()):
+        frame_count += 1
+        print(f"frame count: {frame_count}")
+        ret_A, frame_A = vid_A.cap.read()
+
+        
+        if not ret_A:
+            print("A finished")
+            vid_A_finished = True
+            vid_A.cap.set(cv2.CAP_PROP_POS_FRAMES, 0)
+            ret_A, frame_A = vid_A.cap.read()
+            a.append(frame_A)
+
+        if vid_A_finished: 
+            break
+        
+        # optional for tps
+        frame_A = cv2.resize(frame_A, (1280, 720))
+        if frame_A is None:
+            print("frame is none")
+        a.append(frame_A)
+
+
+        
+    frame_count=0
+
+    print("\n\n\n\nFor B")
+    
+    while(vid_B.cap.isOpened()):
+        
+        frame_count += 1
+        print(f"frame count: {frame_count}")
+        ret_B, frame_B = vid_B.cap.read()
+
+        
+        if not ret_B:
+            print("B finished")
+            vid_B_finished = True
+            vid_B.cap.set(cv2.CAP_PROP_POS_FRAMES, 0)
+            ret_B, frame_B = vid_B.cap.read()
+            b.append(frame_B)
+        if vid_B_finished:
+            break
+
+        frame_B = cv2.resize(frame_B, (1280, 720))
+        b.append(frame_B)
+
+        
+    frame_count=0
+    i=0
+    print("old number of frames:",len(a))
+    print("old number of frames:",len(b))
+    
+    a,b=frame_matcher(a,b)
+    print("new number of frames:",len(a))
+    print("new number of frames:",len(b))
+    
+    out1=cv2.VideoWriter('hello1.avi',cv2.VideoWriter_fourcc('M','J','P','G'), 10, (1280,720))
+    out2=cv2.VideoWriter('hello2.avi',cv2.VideoWriter_fourcc('M','J','P','G'), 10, (1280,720))
+
+    for i in range(len(a)):
+        print("show a",i+1)
+        cv2.imshow("vidA",a[i])
+        out1.write(a[i])
+        print("show b",i+1)
+        cv2.imshow("vidB",b[i])
+        out2.write(b[i])
+        #time.sleep(1)
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            break
+        
+        
+
+    vid_A.release()
+    vid_B.release()
+    cv2.destroyAllWindows()
